d23232f8a877b6871cb9181ba6b79f7003a57774
Tim Cook <twcook@users.noreply.github.com>
2023-02-24 21:40:05 -0600

    Testing & Settings
    Create unitttests for S3MPython config. Add S3MPython conf to settings.py

--- s3modeltools/s3modeltools/settings.py
+++ s3modeltools/s3modeltools/settings.py
@@ -9,11 +9,35 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
+import configparser
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+# ************************************************************************************************
+# S3Model Python configuration file
+
+S3MPYTHON_CONF = os.path.join(BASE_DIR, 'conf', 'S3MPython.conf')
+if os.path.isfile(S3MPYTHON_CONF):
+    config = configparser.ConfigParser()
+    config.read(S3MPYTHON_CONF)
+else:
+    print('\n\nNo S3MPython configuration file found.\n\n')
+    exit()
+
+
+XMLDIR = config['S3MPython']['xmldir']
+RDFDIR = config['S3MPython']['rdfdir']
+DMLIB = config['S3MPython']['dmlib']
+PRJPATH = config['S3MPython']['prjpath']
+ACSFILE = config['S3MPython']['acsfile']
+XML_CATALOG_DIR = config['S3MPython']['catalog']
+# Used by libxml2 (lxml)
+os.environ['XML_CATALOG_FILES'] = XML_CATALOG_DIR
+
+# ************************************************************************************************
 
 
 # Quick-start development settings - unsuitable for production
--- s3modeltools/tests/test_conf.py
+++ s3modeltools/tests/test_conf.py
@@ -1,30 +1,56 @@
 """
-Test that the XML catalog is working correctly.
+Test that the S3MPython config file is correctly found and .
 """
 
 import os
 import configparser
-from django.test import TestCase
+from pathlib import Path
+from unittest import TestCase
 from lxml import etree
+
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 class TestWorkingDir(TestCase):
     """
-    Test that the working directory is set correctly.
+    Test that the working directory is set correctly and the S3MPython configuration is found.
     """
 
     def test_working_dir(self):
         """
         Test that the working directory is set correctly.
         """
-        workdir = os.getcwd()
-        print(f'Working directory: {workdir}\n\n')
-        self.assertTrue(os.path.isfile(os.path.join(workdir,'../conf', 'S3MPython.conf')))
+        self.assertTrue(os.path.isfile(os.path.join(BASE_DIR,'../conf', 'S3MPython.conf')))
 
     def test_xml_catalog(self):
         """
         Test that the XML catalog is set correctly.
         """
-        workdir = os.getcwd()
-        print(f'Working directory: {workdir}\n\n')
-        self.assertTrue(os.path.isfile(os.path.join(workdir,'../conf', 'catalog.xml')))
+        if os.path.isfile(os.path.join(BASE_DIR,'../conf', 'S3MPython.conf')):
+            config = configparser.ConfigParser()
+            config.read(os.path.join(BASE_DIR,'../conf', 'S3MPython.conf'))
+            cat_dir = config['S3MPython']['catalog']
+            cat_file = os.path.join(cat_dir, 'catalog.xml')
+            self.assertTrue(os.path.isfile(cat_file))
+        else:
+            print(f'No S3MPython configuration file found.\n\n')
+            exit()
+
+    def test_rm_schema_location(self):
+        """
+        Test that the XML Schema RM is set correctly.
+        """
+        if os.path.isfile(os.path.join(BASE_DIR,'../conf', 'S3MPython.conf')):
+            config = configparser.ConfigParser()
+            config.read(os.path.join(BASE_DIR,'../conf', 'S3MPython.conf'))
+            cat_dir = config['S3MPython']['catalog']
+            cat_file = os.path.join(cat_dir, 'catalog.xml')
+        else:
+            print(f'No S3MPython configuration file found.\n\n')
         
+        with open(cat_file, 'rb') as f:
+            xml = f.read()
+        root = etree.fromstring(xml)
+        rm_schema_file = root[1].attrib['uri']
+        self.assertTrue(os.path.isfile(rm_schema_file))
+
+     
